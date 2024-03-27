14b132c53ebb65aaad2c1381938b08bcc7edebf0
5innim <bjm22247@gmail.com>
2024-03-27 11:32:12 +0900

    refactor: Change custom method to predefined generic method

--- src/main/java/com/early/stage/demo/global/config/SecurityConfig.java
+++ src/main/java/com/early/stage/demo/global/config/SecurityConfig.java
@@ -5,13 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AnonymousConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.config.annotation.web.configurers.ServletApiConfigurer;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,49 +16,14 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .sessionManagement(this::sessionManagementConfigure)
-            .headers(this::headersConfigure)
-            .csrf(this::csrfConfigure)
-            .logout(this::logoutConfigure)
-            .servletApi(this::servletApiConfigure)
-            .anonymous(this::anonymousConfigure)
-            .exceptionHandling(this::exceptionHandlingConfigure);
+            .sessionManagement(AbstractHttpConfigurer::disable)
+            .headers(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .servletApi(AbstractHttpConfigurer::disable)
+            .anonymous(AbstractHttpConfigurer::disable)
+            .exceptionHandling(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
-
-    private void sessionManagementConfigure(
-        SessionManagementConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void headersConfigure(
-        HeadersConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void csrfConfigure(CsrfConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void logoutConfigure(
-        LogoutConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void servletApiConfigure(
-        ServletApiConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void anonymousConfigure(
-        AnonymousConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
-    private void exceptionHandlingConfigure(
-        ExceptionHandlingConfigurer<HttpSecurity> configure) {
-        configure.disable();
-    }
-
 }
