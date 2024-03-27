8f569ee5b4b030d1f474870a97c963a0bd0f436e
5innim <bjm22247@gmail.com>
2024-03-27 10:59:30 +0900

    feat: Add runner class

---
+++ src/main/java/com/early/stage/demo/global/DemoApplicationRunner.java
@@ -0,0 +1,20 @@
+package com.early.stage.demo.global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DemoApplicationRunner implements ApplicationRunner {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.debug("[active_profile]: " + activeProfile);
+    }
+}
