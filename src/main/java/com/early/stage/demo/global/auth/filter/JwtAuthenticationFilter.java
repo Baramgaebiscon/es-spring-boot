fe0b80b0d3d7c44172a561979aa291f8d72b44e2
5innim <bjm22247@gmail.com>
2024-03-29 17:43:30 +0900

    feat: Add JwtAuthenticationFilter class

---
+++ src/main/java/com/early/stage/demo/global/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.early.stage.demo.global.auth.filter;
+
+import com.early.stage.demo.global.auth.CustomUserDetailsService;
+import com.early.stage.demo.global.error.ErrorStatusException;
+import com.early.stage.demo.global.property.JwtProperty;
+import com.early.stage.demo.global.util.JwtUtil;
+import com.early.stage.demo.global.util.ResponseUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private CustomUserDetailsService customUserDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null || prefixNotMatched(authorization)) {
+            filterChain.doFilter(request, response);
+        } else {
+            String token = authorization.substring(JwtProperty.prefix.length());
+            try {
+                Claims claims = JwtUtil.validateToken(token);
+                if (claims.getSubject().equals("access")) {
+                    Long userId = Long.valueOf((Integer) claims.get("uid"));
+                    Authentication authResult = authenticate(userId);
+                    onSuccessfulAuthentication(authResult, request, response);
+                } else {
+                    filterChain.doFilter(request, response);
+                }
+            } catch (ErrorStatusException ex) {
+                ResponseUtil.setResponseToErrorResponse(response, ex.getErrorCase());
+            }
+        }
+    }
+
+    private boolean prefixNotMatched(String value) {
+        return !value.startsWith(JwtProperty.prefix);
+    }
+
+    private Authentication authenticate(Long userId) throws ErrorStatusException {
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUserId(
+            userId);
+        return new UsernamePasswordAuthenticationToken(
+            userDetails, null, userDetails.getAuthorities());
+    }
+
+    private void onSuccessfulAuthentication(Authentication authResult, HttpServletRequest request,
+        HttpServletResponse response) {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authResult);
+    }
+
+
+}
