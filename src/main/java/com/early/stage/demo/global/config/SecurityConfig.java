f61c6b20ff778c50434ed4bf4d9bb5661fe38932
5innim <bjm22247@gmail.com>
2024-03-29 11:47:17 +0900

    feat: Add FormDataLoginAuthentication Filter at security filter chain

--- src/main/java/com/early/stage/demo/global/config/SecurityConfig.java
+++ src/main/java/com/early/stage/demo/global/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package com.early.stage.demo.global.config;
 
+import com.early.stage.demo.domain.member.service.MemberLoginService;
+import com.early.stage.demo.global.auth.filter.FormDataLoginAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final MemberLoginService memberLoginService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -28,8 +35,20 @@ public class SecurityConfig {
             .logout(AbstractHttpConfigurer::disable)
             .servletApi(AbstractHttpConfigurer::disable)
             .anonymous(AbstractHttpConfigurer::disable)
-            .exceptionHandling(AbstractHttpConfigurer::disable);
+            .exceptionHandling(AbstractHttpConfigurer::disable)
+            .with(new CustomDsl(), dsl -> {
+            });
 
         return http.build();
+    }
+
+    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.addFilterAt(new FormDataLoginAuthenticationFilter(http.getSharedObject(
+                    AuthenticationManager.class), memberLoginService),
+                UsernamePasswordAuthenticationFilter.class);
+        }
     }
 }
