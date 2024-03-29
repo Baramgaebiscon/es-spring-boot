af86f566ad35a1fdc65fa8c4d7f8939494c4fa84
5innim <bjm22247@gmail.com>
2024-03-29 17:43:05 +0900

    feat: Add loadUserByUserId method

--- src/main/java/com/early/stage/demo/global/auth/CustomUserDetailsService.java
+++ src/main/java/com/early/stage/demo/global/auth/CustomUserDetailsService.java
@@ -2,18 +2,15 @@ package com.early.stage.demo.global.auth;
 
 import com.early.stage.demo.domain.member.entity.Member;
 import com.early.stage.demo.global.auth.enums.Role;
-import lombok.RequiredArgsConstructor;
+import com.early.stage.demo.global.error.ErrorStatusException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-
-    private final PasswordEncoder passwordEncoder;
 
     /**
      * In this demo project, use dummy data model
@@ -24,7 +21,21 @@ public class CustomUserDetailsService implements UserDetailsService {
             Member.builder()
                 .memberId(1L)
                 .id(username)
-                .password(passwordEncoder.encode("12345**"))
+                .password((new BCryptPasswordEncoder()).encode("12345**"))
+                .role(Role.USER)
+                .build()
+        );
+    }
+
+    /**
+     * In this demo project, use dummy data model
+     */
+    public UserDetails loadUserByUserId(Long userId) throws ErrorStatusException {
+        return new CustomUserDetails(
+            Member.builder()
+                .memberId(userId)
+                .id("test_id")
+                .password((new BCryptPasswordEncoder()).encode("12345**"))
                 .role(Role.USER)
                 .build()
         );
