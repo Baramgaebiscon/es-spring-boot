34e9d0548da1cbe8f8e54783f33f9ea9a5b4805e
5innim <bjm22247@gmail.com>
2024-03-29 11:43:17 +0900

    feat: Add YamlLoadFactory for read yaml property file

--- src/main/java/com/early/stage/demo/global/DemoApplicationRunner.java
+++ src/main/java/com/early/stage/demo/global/DemoApplicationRunner.java
@@ -1,16 +1,19 @@
 package com.early.stage.demo.global;
 
+import com.early.stage.demo.global.common.YamlLoadFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@PropertySource(value = "classpath:/property/jwt.yml", factory = YamlLoadFactory.class)
 public class DemoApplicationRunner implements ApplicationRunner {
 
-    @Value("${spring.profiles.active}")
+    @Value("#{environment['spring.profiles.active']}")
     private String activeProfile;
 
     @Override
