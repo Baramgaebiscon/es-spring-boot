19cfbcc27e1e309d1b88e57575998daea0b11245
5innim <bjm22247@gmail.com>
2024-03-30 14:54:44 +0900

    feat: Add error case for not most recent generated refresh token

--- src/main/java/com/early/stage/demo/global/error/ErrorCase.java
+++ src/main/java/com/early/stage/demo/global/error/ErrorCase.java
@@ -11,6 +11,8 @@ public enum ErrorCase {
     _401_LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "401001", "login fail"),
     _401_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "401002", "token is expired"),
     _401_TOKEN_VALIDATE_FAIL(HttpStatus.UNAUTHORIZED, "401003", "token validation is fail"),
+    _401_NOT_MOST_RECENT_GENERATED_TOKEN(HttpStatus.UNAUTHORIZED, "401004",
+        "this refresh token is not most recent generated"),
     _500_JWT_GENERATE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "500001", "jwt token generation fail");
     private final HttpStatus status;
     private final String code;
